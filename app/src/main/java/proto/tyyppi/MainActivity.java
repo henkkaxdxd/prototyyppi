ba9d7553aa59fe560fce5fe3aae9e0b71bd19ae7
henkkaxdxd <tgntracks@hotmail.com>
2017-05-29 13:14:05 +0300

    Updated.

--- app/src/main/java/proto/tyyppi/MainActivity.java
+++ app/src/main/java/proto/tyyppi/MainActivity.java
@@ -1,13 +1,18 @@
 package proto.tyyppi;
 
+        import android.content.SharedPreferences;
         import android.os.AsyncTask;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.text.Editable;
+        import android.text.TextWatcher;
         import android.view.View;
         import android.widget.AdapterView;
         import android.widget.ArrayAdapter;
-        import android.widget.Spinner;
+        import android.widget.AutoCompleteTextView;
+        import android.widget.Button;
         import android.widget.TextView;
+        import android.widget.Toast;
 
         import java.io.BufferedReader;
         import java.io.IOException;
@@ -22,10 +27,14 @@ public class MainActivity extends AppCompatActivity {
     private TextView tvData;
     private TextView groupV;
 
-    String beaconMajor = "123456";
-    String groupID = "nulll";
+    AutoCompleteTextView autoComp;
+    String[] Groups;
 
-    //String urlStr = "https://oven-sausage.herokuapp.com/add/"+beaconMajor+"/"+groupID;
+    String beaconMajor = "123456";
+    String groupID = "NotSet";
+    String GroupString;
+
+    Button registerBtn, testBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,24 +43,70 @@ public class MainActivity extends AppCompatActivity {
 
         tvData = (TextView) findViewById(R.id.textView);
         groupV = (TextView) findViewById(R.id.groupView);
-        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
 
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.ryhma_array));
-        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mySpinner.setAdapter(myAdapter);
+        registerBtn = (Button) findViewById(R.id.register);
+        testBtn = (Button) findViewById(R.id.button);
 
-        mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        SharedPreferences sharedPref= getSharedPreferences("mypref", MODE_PRIVATE);
+        groupID = sharedPref.getString("savedGroup", groupID);
+
+        if (groupV == null) {
+            groupV.setText("Et ole vielä missään ryhmässä. Valitse ryhmä.");
+        } else {
+            groupV.setText("Olet ryhmässä: " + groupID + "\nJos haluat vaihtaa ryhmää syötä uusi ryhmätunnus.");
+        }
+
+        autoComp = (AutoCompleteTextView)findViewById(R.id.autoCompleteTextView);
+        Groups = getResources().getStringArray(R.array.groups);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Groups);
+        autoComp.setAdapter(adapter);
+
+        autoComp.addTextChangedListener(new TextWatcher() {
+
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                groupID = "toimii";
-                groupV.setText(groupID);
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // TODO Auto-generated method stub
+
+                registerBtn.setVisibility(View.GONE);
+                testBtn.setVisibility(View.GONE);
+
+                if(!autoComp.isPerformingCompletion())
+                {
+                    boolean validator=false;
+                    for(String val :  Groups )
+                    {
+                        if(val.toLowerCase().startsWith(s.toString().toLowerCase()))
+                        {
+                            //Toast.makeText(MainActivity.this, "toLowerCase", Toast.LENGTH_SHORT).show();
+                            validator=true;
+                        }
+                    }
+
+                    if(!validator)
+                    {
+                        Toast.makeText(MainActivity.this, "Oho! Kirjoitusvirhe.", Toast.LENGTH_SHORT).show();
+                        autoComp.setText("");
+                    }
+                }
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                groupID = "eitoimi";
-                groupV.setText(groupID);
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+        autoComp.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "onItemClick", Toast.LENGTH_SHORT).show();
+
+                registerBtn.setVisibility(View.VISIBLE);
             }
         });
 
@@ -64,6 +119,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void click(View v) {
         new JSONtask().execute("https://oven-sausage.herokuapp.com/add/"+beaconMajor+"/"+groupID);
+    }
+
+    public void registerClick(View v){
+        GroupString = autoComp.getText().toString();    // Otetaan listasta nimi
+        groupID = GroupString;                          // Asetetaan se
+        groupV.setText("Olet ryhmässä: " + groupID + "\nJos haluat vaihtaa ryhmää syötä uusi ryhmätunnus.");    // Tulostetaan se
+
+        testBtn.setVisibility(View.VISIBLE);
+        saveGroup("savedGroup", groupID);
     }
 
     public class JSONtask extends AsyncTask<String, String, String> {
@@ -116,5 +180,16 @@ public class MainActivity extends AppCompatActivity {
 
             tvData.setText(result);
         }
+    }
+
+    public void saveGroup(String name, String addGroup){
+        // Create object of SharedPreferences.
+        SharedPreferences sharedPref= getApplicationContext().getSharedPreferences("mypref", MODE_PRIVATE);
+        //now get Editor
+        SharedPreferences.Editor editor= sharedPref.edit();
+        //put your value
+        editor.putString(name, addGroup);
+        //commits your edits
+        editor.commit();
     }
 }
